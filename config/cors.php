05d744dfdd0882eff7139eaa3c0057a2f90fdee6
Văn phung Manh <manhsubcheo2@gmail.com>
2024-06-27 21:30:01 +0700

    fix

--- config/cors.php
+++ config/cors.php
@@ -17,7 +17,7 @@ return [
 
     'paths' => ['api/*', '*'],  // Đường dẫn nào cần CORS, 'api/*' là phổ biến cho các ứng dụng API
     'allowed_methods' => ['*'],  // Phương thức nào được phép
-    'allowed_origins' => ['*'],  // Nguồn gốc nào được phép, '*' nghĩa là tất cả
+    'allowed_origins' => ['http://testfe.prodevecode.com'],  // Nguồn gốc nào được phép, '*' nghĩa là tất cả
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['Content-Type', 'X-Requested-With', 'Authorization'],
     'exposed_headers' => [],
